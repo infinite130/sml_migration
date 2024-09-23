56b1c6896a2393da653f3c023ac1b31a1f442a04
infinite130 <skfo3219@gmail.com>
2024-09-23 15:35:27 +0900

    initial commit

---
+++ src/main/java/com/narae/sml/SmlMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.narae.sml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmlMigrationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SmlMigrationApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/narae/sml/SmlMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narae.sml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmlMigrationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
